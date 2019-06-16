8aa4551447e5b8db8f273f0abb20f15989f7357e
macbook <l.n.ammouri@gmail.com>
2019-06-16 16:07:58 +0300

    modified the config file

--- src/config/sp_mawdoo3_laravel.php
+++ src/config/sp_mawdoo3_laravel.php
@@ -2,7 +2,7 @@
 
 return [
     'google_custom_search_api' => [
-        'api_key' => 'AIzaSyBCgmxs5lAkAfOHEk0xGkA4zhriHRJKgz8',
-        'engine_id' => '009172838475775797497:eqgd2rsemvk',
+        'api_key' => 'YOUR_API_KEY',
+        'engine_id' => 'YOUR_SEARCH_ENGINE_ID',
     ]
 ];
